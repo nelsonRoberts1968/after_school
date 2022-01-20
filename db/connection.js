99829cd2ced3db58b009a6f01ba82d777ec81bdf
Nelson Roberts <nroberts@sbmcorp.com>
2022-01-19 18:38:16 -0800

    more canges

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,38 @@
 
-const mysql = require("mysql2");
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// Connect to database
-const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: '',
-  database: 'clubhubdb'
-});
+let sequelize;
 
-module.exports = db;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
+  sequelize = new Sequelize(
+   "clubhubdb",
+    "root",
+    "",
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+};
+
+module.exports = sequelize;// const mysql = require("mysql2");
+
+// // Connect to database
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   // Your MySQL username,
+//   user: 'root',
+//   // Your MySQL password
+//   password: 'password',
+//   database: 'clubhubdb'
+// });
+
+// module.exports = db;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -37,16 +38,57 @@ app.use(routes);
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-// sequelize.sync({ force: false }).then(() => {//
-//start server after DB Connection
-db.connect(err =>{
-    if(err)throw err;
-    console.log('Database Connected');
- app.listen(PORT, () => console.log('Now listening'));
-});
- // render about.handlebars
-// app.get('./view/about', (req, res) => {
+
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const SequelizeStore = require('connect-session-sequelize') (session.Store);
+
+// const routes = require('./controllers');
+// const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {}, 
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
+
+// app.use(session(sess));
+
+// const hbs = exphbs.create({ helpers });
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+// sequelize.sync({ force: true }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+// // sequelize.sync({ force: false }).then(() => {//
+// //start server after DB Connection
+// db.connect(err =>{
+//     if(err)throw err;
+//     console.log('Database Connected');
+//  app.listen(PORT, () => console.log('Now listening'));
+// });
+
+//  // render about.handlebars
+// // app.get('./view/about', (req, res) => {
         
-//         res.render('main', {layout: 'about'});
-//     });
+// //         res.render('main', {layout: 'about'});
+// //     });
        
