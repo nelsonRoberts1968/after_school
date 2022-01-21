b166d6523c8ffc081437681281baea57e65d62cf
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-01-21 15:42:45 -0800

    api routes working correctly. push to base repo

--- controllers/api/course-routes.js
+++ controllers/api/course-routes.js
@@ -2,35 +2,14 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Course, Age, Category, Location } = require('../../models');
 
-//test to see if I can get it to return the data in a handlebar template. If it doesn't work, revert to below
-router.get('/', async (req, res) => {
-  Course.findAll({
-    attributes: ['id', 'course_title', 'description', 'url'],
-    include: [
-      {
-        model: Course,
-        attributes: ['id', 'course_title', 'description', 'url'],
-      },
-    ],
-  })
-    .then((dbCourseData) => {
-      // pass a single course object into the homepage template
-      res.render('courses', dbCourseData.get({ plain: true }));
-    })
+// //returns all courses as object /api/courses
+router.get('/', (req, res) => {
+  Course.findAll()
+    .then((dbCourseData) => res.json(dbCourseData))
     .catch((err) => {
-      console.log(err);
       res.status(500).json(err);
     });
 });
-
-// //returns course api DATA!!
-// router.get('/', (req, res) => {
-//   Course.findAll()
-//     .then((dbCourseData) => res.json(dbCourseData))
-//     .catch((err) => {
-//       res.status(500).json(err);
-//     });
-// });
 
 //GET /api/courses/1
 router.get('/:id', (req, res) => {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const courseRoutes = require('./course-routes');
 
+
 router.use('/courses', courseRoutes);
 
 module.exports = router;
