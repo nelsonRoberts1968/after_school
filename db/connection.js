0975d1f6620d1961a8635207b3af6171edb51557
Juanjohn11 <elmatanaco1144@gmail.com>
2022-01-20 21:11:26 -0800

    added JAWSDB

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config()
 const mysql = require("mysql2");
 
 // Connect to database
-const db = mysql.createConnection({
+const db = mysql.createConnection(process.env.JAWSDB_URL||{
   host: 'localhost',
   // Your MySQL username,
   user: 'root',
