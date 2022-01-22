33e37a9657e5a5ff66cff472628ec6aa38809049
Nelson Roberts <nroberts@sbmcorp.com>
2022-01-22 14:00:21 -0800

    fixing merge conflict on develop

--- db/connection.js
+++ db/connection.js
@@ -23,16 +23,4 @@ if (process.env.JAWSDB_URL) {
   );
 };
 
-module.exports = sequelize;// const mysql = require("mysql2");
-
-// // Connect to database
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   // Your MySQL username,
-//   user: 'root',
-//   // Your MySQL password
-//   password: 'password',
-//   database: 'clubhubdb'
-// });
-
-// module.exports = db;
+module.exports = sequelize;
