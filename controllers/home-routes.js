8e117af071ad82c27aa80bdd89f0509e9679ada5
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-01-21 23:24:53 -0800

    courses handlebar updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,19 +5,23 @@ const { Account, Category, Course, Location, User, Age } = require('../models');
 //Import the custom middleware
 const withAuth = require('../utils/auth');
 
+// render homepage
+router.get('/', async (req, res) => {
+  res.render('homepage');
+});
+
 // router.get('/signup', async (req, res) => {
 //   res.render('signup');
 // });
+
+router.get('/courses', (req, res) => {
+  res.render('courses');
+});
 
 //render new event page
 //layout works
 router.get('/submit', async (req, res) => {
   res.render('newEvent');
-});
-
-// render homepage
-router.get('/', async (req, res) => {
-  res.render('homepage');
 });
 
 module.exports = router;
