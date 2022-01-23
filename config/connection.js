abf536e369889e14fd816be5c16dcd251e3e46a1
Nelson Roberts <nroberts@sbmcorp.com>
2022-01-22 19:25:32 -0800

    fixing merge conflict

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,4 @@
 const Sequelize = require('sequelize');
-<<<<<<< HEAD
-require('dotenv').config();
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
-  sequelize = new Sequelize(
-   "clubhubdb",
-    "root",
-    "",
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    }
-  );
-};
-=======
 
 require('dotenv').config();
 
@@ -34,6 +10,5 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
->>>>>>> Nedabranch
 
 module.exports = sequelize;
