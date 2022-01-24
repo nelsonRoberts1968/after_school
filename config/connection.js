18df484e7e8c48c11329807062a556fb515646e9
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-01-24 00:21:36 -0800

    fixed heroku connection issues

--- config/connection.js
+++ config/connection.js
@@ -2,23 +2,24 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Connect to database
-const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: 'password',
-  database: 'clubhubdb'
-});
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   // Your MySQL username,
+//   user: 'root',
+//   // Your MySQL password
+//   password: '',
+//   database: 'clubhubdb'
+// });
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
-  sequelize = new Sequelize(
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
+
+  const sequelize = new Sequelize(
    "clubhubdb",
     "root",
-    "password",
+    "",
     // process.env.DB_NAME,
     // process.env.DB_USER,
     // process.env.DB_PASSWORD,
@@ -28,6 +29,5 @@ if (process.env.JAWSDB_URL) {
       port: 3306,
     }
   );
-};
 
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,7 @@ const sequelize = require('../config/connection');
 const router = require('express').Router();
 const { Account, Category, Course, Location, User, Age } = require('../models');
 
+//render all courses
 router.get('/courses', (req, res) => {
   res.render('courses');
 });
@@ -23,13 +24,15 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+//render signup page
 router.get('/signup',(req, res) => {
   res.render('signup');
 });
 
-router.get('./view/about', (req, res) => {
-  res.render('main', { layout: 'about' });
-const withAuth = require('./auth');
+//
+// router.get('./view/about', (req, res) => {
+//   res.render('main', { layout: 'about' });
+// const withAuth = require('./auth');
 
 
 // render homepage
@@ -41,13 +44,19 @@ router.get('/about', async (req, res) => {
     res.render('about');
 });
 //login
-router.get('/login', async (req, res) => {
-    res.render('login');
+// router.get('/login', async (req, res) => {
+//     res.render('login');
+// });
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  };
 });
 
-router.get('/about',(req,res)=>{
-  res.render('about');
-});
+// router.get('/about',(req,res)=>{
+//   res.render('about');
+// });
 //get all courses
 router.get('/courses', async (req, res) => {
   try {
@@ -72,60 +81,14 @@ router.get('/courses/:id', withAuth, async (req, res) => {
 
     const courses = dbCourseData.get({ plain: true });
 
-    res.render('courses', { course, loggedIn: req.session.loggedIn });
+    res.render('courses', { courses, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
-module.exports = router;
-    try {
-        const dbCourseData = await Course.findAll({ include: [{ all: true, nested: true }]});
-        dbCourseData.map((Course) => {
-            Course.get({ plain: true})
-        });
-        res.render('courses');
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
-
-
-// GET course by name
-// Use the custom middleware before allowing the user to access the course
-// router.get('/courses/:title', withAuth, async (req, res) => {
-//   try {
-//     const dbCourseData = await Course.findByPk(req.params.id);
-
-//     const courses = dbCourseData.get({ plain: true });
-
-//     res.render('courses', { course, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-
-//   res.render('login');
-// });
-//login
+//render featured
 router.get('/featured', async (req, res) => {
     res.render('presentCourses');
 });
